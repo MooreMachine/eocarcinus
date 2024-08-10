8739e091294a81f553b72a212dffe3e9b1c050d7
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2024-08-10 20:43:14 +0200

    Change visibility to hide private methods

--- src/align_comments/modify_whitespace.rs
+++ src/align_comments/modify_whitespace.rs
@@ -1,4 +1,4 @@
-pub(crate) fn remove_trailing_whitespace(s: &str) -> &str {
+pub(super) fn remove_trailing_whitespace(s: &str) -> &str {
     let characters: Vec<char> = s.chars().collect();
     let mut count = 0;
     for c in characters.iter().rev() {
@@ -12,7 +12,7 @@ pub(crate) fn remove_trailing_whitespace(s: &str) -> &str {
     text
 }
 
-pub(crate) fn remove_leading_whitespace(s: &str) -> &str {
+pub(super) fn remove_leading_whitespace(s: &str) -> &str {
     let count = s.find(|c: char| !c.is_whitespace()).unwrap_or_default();
     s.split_at(count).1
 }
