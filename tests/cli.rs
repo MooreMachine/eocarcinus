a4750d5c6ae43808ac6d2d76d3ee63801e90dab9
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2024-08-11 21:18:36 +0200

    Add integration test

---
+++ tests/cli.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
+use predicates::prelude::predicate;
+
+#[test]
+fn file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("eocarcinus")?;
+
+    cmd.arg("--path").arg("path/to/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
