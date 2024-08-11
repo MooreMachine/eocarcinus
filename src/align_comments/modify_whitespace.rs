207feaf55531e4b45e008df54feab587d31572c6
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2024-08-11 17:07:21 +0200

    Wrap trim_start and trim_end functions

--- src/align_comments/modify_whitespace.rs
+++ src/align_comments/modify_whitespace.rs
@@ -1,20 +1,9 @@
 pub(super) fn remove_trailing_whitespace(s: &str) -> &str {
-    let characters: Vec<char> = s.chars().collect();
-    let mut count = 0;
-    for c in characters.iter().rev() {
-        if !c.is_whitespace() {
-            break;
-        }
-        count += 1;
-    }
-    let trailing_whitespace_index = s.len() - count;
-    let (text, _trailing_whitespace) = s.split_at(trailing_whitespace_index);
-    text
+    s.trim_end()
 }
 
 pub(super) fn remove_leading_whitespace(s: &str) -> &str {
-    let count = s.find(|c: char| !c.is_whitespace()).unwrap_or_default();
-    s.split_at(count).1
+    s.trim_start()
 }
 
 #[cfg(test)]
